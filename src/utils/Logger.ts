735c80b2ad0f664b12ed7fe60f10eb0039904135
Richard Martins <me@richaar.dev>
2026-05-19 12:50:30 -0300

    chore: use pino instead a custom logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,40 +1,31 @@
-import {
-  bgBlueBright,
-  bgGreenBright,
-  bgRed,
-  bgYellowBright,
-  black,
-  blueBright,
-  greenBright,
-  red,
-  yellow,
-  yellowBright,
-} from "chalk";
-import moment from "moment";
+import pino from "pino";
 
-export default class Logger {
-  static get currentTime() {
-    return `${yellow(moment(Date.now()).format("[[]DD/MM/YYYY hh:mm:ss[]]"))}`;
-  }
+const logger = pino();
+export default logger;
 
-  static generateLog(logType: string, message: any) {
-    console.log(`${Logger.currentTime} ${logType}: ${message}`);
-  }
-
-  static warn(message: any): void {
-    Logger.generateLog(bgYellowBright(black("[WARN]")), yellowBright(message));
-  }
-
-  static error(message: any, isFatal?: boolean) {
-    Logger.generateLog(bgRed(black(isFatal ? "[FATAL ERROR]" : "[ERROR]")), red(message));
-  }
-
-  static debug(message: any) {
-    if (process.env.production === "true") return;
-    Logger.generateLog(bgBlueBright(black("[DEBUG]")), blueBright(message));
-  }
-
-  static info(message: any) {
-    Logger.generateLog(bgGreenBright(black("[INFO]")), greenBright(message));
-  }
-}
+// export default class Logger {
+//   static get currentTime() {
+//     return `${yellow(moment(Date.now()).format("[[]DD/MM/YYYY hh:mm:ss[]]"))}`;
+//   }
+//
+//   static generateLog(logType: string, message: any) {
+//     console.log(`${Logger.currentTime} ${logType}: ${message}`);
+//   }
+//
+//   static warn(message: any): void {
+//     Logger.generateLog(bgYellowBright(black("[WARN]")), yellowBright(message));
+//   }
+//
+//   static error(message: any, isFatal?: boolean) {
+//     Logger.generateLog(bgRed(black(isFatal ? "[FATAL ERROR]" : "[ERROR]")), red(message));
+//   }
+//
+//   static debug(message: any) {
+//     if (process.env.production === "true") return;
+//     Logger.generateLog(bgBlueBright(black("[DEBUG]")), blueBright(message));
+//   }
+//
+//   static info(message: any) {
+//     Logger.generateLog(bgGreenBright(black("[INFO]")), greenBright(message));
+//   }
+// }
